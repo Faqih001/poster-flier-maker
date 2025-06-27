f3c549675a849dc077a5762e629dfd0a3daefc08
Amirul Faqih <fakiiahmad001@gmail.com>
2025-06-28 01:13:57 +0300

    feat: Add route for templates-gallery page

--- src/App.tsx
+++ src/App.tsx
@@ -32,6 +32,7 @@ const App = () => (
             <Route path="/" element={<Index />} />
             <Route path="/auth" element={<Auth />} />
             <Route path="/templates" element={<TemplatesGallery />} />
+            <Route path="/templates-gallery" element={<TemplatesGallery />} />
             <Route path="/create" element={<CreatePoster />} />
             <Route path="/dashboard" element={<Dashboard />} />
             <Route path="/order-prints" element={<OrderPrints />} />
